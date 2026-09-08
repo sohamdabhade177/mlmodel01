51a68495e455f97b9b9833f5546c58a7fd725597
Soham Dabhade <sohamdabhade177@gmail.com>
2026-09-08 15:52:46 +0530

    update train.py for random forest

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 print("Hello, World!")
 print("Student performence prediction")
 print("loading data set")
-print("training decision tree Model")
+print("training random forest Model")
 print("model taining completed")
 
 
